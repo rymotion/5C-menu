7dcfa953404fc79b558f5526147575bf8fe46db4
rymotion <rymotion1@me.com>
2016-11-11 23:36:45 -0800

    NSDate
    NSDate needs to be fixed as it does need to be reset from the UTC

--- 5C menu/FirstViewController.swift
+++ 5C menu/FirstViewController.swift
@@ -10,14 +10,16 @@ import UIKit
 import JavaScriptCore
 import WebKit
 
+var date:String = ""
+
 class FirstViewController: UIViewController {
-    
-    
-    @IBOutlet var school: UIView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print(getCurrentDate())
+        //webDataCall()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,9 +27,18 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    //func getFoodData() -> NSMutableArray {
+    func getCurrentDate() -> String {
+        //  Get the current date of the user and return it to be used for the parsing data
+        let dateComponent = NSDateComponents()
+        let day = dateComponent.day
+        let month = dateComponent.month
+        let year = dateComponent.year
         
-    //}
+        date = "\(month)+\(day)+\(year)"
+        print(date)
+        
+        return date
+    }
 
     func webDataCall() -> Void {
         if let url = URL(string:"https://aspc.pomona.edu/menu/"){
